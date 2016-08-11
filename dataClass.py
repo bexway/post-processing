76ae96b934f2e37f11b92aa836ae1805b6856848
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2016-08-11 11:41:20 -0500

    Started multi-archiving

--- dataClass.py
+++ dataClass.py
@@ -7,7 +7,7 @@ import csv
 
 class dataManager():
 	def __init__(self, directory):
-		self.data = []
+		data = []
 		#Getting all the filenames from the directory
 		self.directory = directory
 		tempfiles = [f for f in listdir(self.directory) if isfile(join(directory, f))]
@@ -23,8 +23,11 @@ class dataManager():
 			reader = csv.DictReader(tempfile)
 			headers = reader.fieldnames
 			for row in reader:
-				self.data += [row]
+				data += [row]
 			tempfile.close()
+
+		self.archive = {"init_total_data":[data, headers]}
+		self.active_data = self.archive["init_total_data"]
 
 		return
 
@@ -39,7 +42,14 @@ class dataManager():
 			return
 
 	def display_files(self):
-		print: "The files that have been read are:"
+		print "The files that have been read are:"
 		for f in self.files:
 			print f
 		return
+
+	def clear_archive(self):
+		data = self.archive["init_total_data"][0]
+		headers = self.archive["init_total_data"][0]
+		self.archive = {"init_total_data":[data, headers]}
+		self.active_data = self.archive["init_total_data"]
+		print "Archives cleared! All that's left is the initial archive of all files' data."
